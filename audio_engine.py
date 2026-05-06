d3d53d47eea9a25bc4bac199b234e781fa1dbc49
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 14:13:25 +0530

    Update audio_engine.py

--- audio_engine.py
+++ audio_engine.py
@@ -1,13 +1,30 @@
 import sounddevice as sd
 import numpy as np
+import librosa
 
 SAMPLE_RATE = 16000
-DURATION = 2
+DURATION = 3
 
-def get_audio():
-    audio = sd.rec(int(DURATION * SAMPLE_RATE),
-                   samplerate=SAMPLE_RATE,
-                   channels=1,
-                   dtype='float32')
+def record_audio():
+
+    print("Recording...")
+
+    audio = sd.rec(
+        int(DURATION * SAMPLE_RATE),
+        samplerate=SAMPLE_RATE,
+        channels=1,
+        dtype='float32'
+    )
+
     sd.wait()
+
     return np.squeeze(audio)
+
+def load_demo_audio(path):
+
+    audio, sr = librosa.load(
+        path,
+        sr=SAMPLE_RATE
+    )
+
+    return audio
