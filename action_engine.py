348c07ae467b401933d4257b346b26a5cdd9d2a8
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 15:52:44 +0530

    Update action_engine.py

--- action_engine.py
+++ action_engine.py
@@ -1,35 +1,14 @@
-import os
-import scipy.io.wavfile as wav
-import time
+from utils import save_audio
 
-def trigger_action(
-    audio,
-    decision
-):
+def trigger_action(audio, decision):
 
     if decision["threat_score"] >= 60:
 
-        if not os.path.exists("recording"):
-
-            os.makedirs("recording")
-
-        filename = f"recording/{time.time()}.wav"
-
-        wav.write(
-            filename,
-            16000,
-            (audio * 32767).astype("int16")
-        )
+        save_audio(audio)
 
         print("\nEMERGENCY AUDIO SAVED")
 
-        with open(
-            "incident_log.txt",
-            "a"
-        ) as f:
-
+        with open("incident_log.txt", "a") as f:
             f.write(
-                f"{time.ctime()} | "
-                f"{decision['alert_level']} | "
-                f"{decision['threat_score']}\n"
+                f"{decision['alert_level']} | {decision['threat_score']}\n"
             )
