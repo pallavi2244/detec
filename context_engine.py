7818a535fd1a1a35ec4f03617aa48e43efad0f83
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 12:20:58 +0530

    Update context_engine.py

--- context_engine.py
+++ context_engine.py
@@ -1,1 +1,15 @@
+context_scores = []
 
+def update_context(score):
+    context_scores.append(score)
+
+    if len(context_scores) > 5:
+        context_scores.pop(0)
+
+def get_context_score():
+    return sum(context_scores)
+
+def detect_pattern():
+    if len(context_scores) < 3:
+        return False
+    return context_scores[-1] > context_scores[-2] > context_scores[-3]
