2e0f5d96381363b57b90be72109a81932ca80c57
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 12:19:25 +0530

    Update audio_engine.py

--- audio_engine.py
+++ audio_engine.py
@@ -1,1 +1,13 @@
+import sounddevice as sd
+import numpy as np
 
+SAMPLE_RATE = 16000
+DURATION = 2
+
+def get_audio():
+    audio = sd.rec(int(DURATION * SAMPLE_RATE),
+                   samplerate=SAMPLE_RATE,
+                   channels=1,
+                   dtype='float32')
+    sd.wait()
+    return np.squeeze(audio)
