d66b2517b8e5d269086846c09cb7588b8d59e5d5
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 15:51:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
-def print_banner():
+import os
+import scipy.io.wavfile as wav
+import time
 
+def save_audio(audio):
+
+    # ✅ AUTO-CREATE FOLDER
+    if not os.path.exists("recording"):
+        os.makedirs("recording")
+
+    filename = f"recording/{time.time()}.wav"
+
+    wav.write(
+        filename,
+        16000,
+        (audio * 32767).astype("int16")
+    )
+
+def print_banner():
     print("=" * 60)
     print(" AI DISTRESS DETECTION SYSTEM ")
     print("=" * 60)
-    
