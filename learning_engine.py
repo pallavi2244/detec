bd0ebff2f8933938c057cda0cbe4f498f14b02a2
PALLAVI <pallavi.bsmh@gmail.com>
2026-05-06 12:21:58 +0530

    Update learning_engine.py

--- learning_engine.py
+++ learning_engine.py
@@ -1,1 +1,17 @@
+import json
 
+try:
+    with open("panic_words.json") as f:
+        PANIC_WORDS = set(json.load(f))
+except:
+    PANIC_WORDS = set(["help", "stop", "save me"])
+
+def learn(text, score):
+    if score > 70:
+        for w in text.split():
+            if len(w) > 3:
+                PANIC_WORDS.add(w.lower())
+
+def save_words():
+    with open("panic_words.json", "w") as f:
+        json.dump(list(PANIC_WORDS), f)
